d6114f30b671a8152d855ea87af990d8a6bb6157
Vajeed Shaikh <vajeedshaikh82@gmail.com>
2026-04-25 19:31:04 +0530

    Remove duplicate PORT and app.listen blocks from server.js

--- server.js
+++ server.js
@@ -219,14 +219,3 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
