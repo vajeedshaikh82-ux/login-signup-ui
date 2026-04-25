9c8e1a259e9ebc2179c8d61f81219c48647ac462
vajeedshaikh82-ux <vajeedshaikh82@gmail.com>
2026-04-25 16:22:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -225,8 +225,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
